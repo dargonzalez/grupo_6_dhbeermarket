c010d0305223788406f549caf482a7e49828d9a4
Dario Gonzalez <dargonzalez@gmail.com>
2022-05-04 14:54:53 -0300

    cambios en index y en footer y header

--- dHBeermarketWeb/app.js
+++ dHBeermarketWeb/app.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 app.use( express.static('public') );
 
-app.listen( 3000, () => console.log('port 3000 ready') );
+app.listen( 3030, () => console.log('port 3030 ready') );
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve('views/index.html'));
